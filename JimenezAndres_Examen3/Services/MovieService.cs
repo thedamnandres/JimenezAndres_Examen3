0cd2d570c7e7af558492e7622276714610e8c2b8
Andres Jimenez <andres_jimenez666@hotmail.com>
2025-01-21 07:38:34 -0500

    Movies VM

---
+++ JimenezAndres_Examen3/Services/MovieService.cs
@@ -0,0 +1,27 @@
+using SQLite;
+using Movie = JimenezAndres_Examen3.Models.Movie;
+using Path = System.IO.Path;
+
+namespace JimenezAndres_Examen3.Services;
+
+public class MovieService 
+{ 
+    private readonly SQLiteConnection _database;
+    
+    public MovieService()
+    {
+        var dbPath = Path.Combine(FileSystem.AppDataDirectory, "jimenezandres_movies.db");
+        _database = new SQLiteConnection(dbPath);
+        _database.CreateTable<Movie>();
+    }
+    
+    public void AddMovie(Movie movie)
+    {
+        _database.Insert(movie);
+    }
+    
+    public List<Movie> GetMovies()
+    {
+        return _database.Table<Movie>().ToList();
+    }
+}
---
+++ JimenezAndres_Examen3/ViewModels/MovieViewModel.cs
@@ -0,0 +1,8 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace JimenezAndres_Examen3.ViewModels;
+
+public class MovieViewModel : ObservableObject
+{
+   
+}
